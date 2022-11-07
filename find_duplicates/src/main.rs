1e4c85c7b940362f8e6b914798edeaf6be0c3d99
Toma Adrian Ionut <sirhadrian21@gmail.com>
2022-11-07 21:34:36 +0200

    todo

--- find_duplicates/src/main.rs
+++ find_duplicates/src/main.rs
@@ -1,9 +1,16 @@
 use env_logger;
 use log::debug;
 use md5::Digest;
-use std::{collections::HashSet, env, ffi, fs, io};
+use std::{fs, io};
+
+
+// Get all the files in a directory
+// Calculate md5 sum for one file
+// Insert the file in hashset
+// Put the duplicates in a separate list
 
 fn main() {
 
+    
     
 }
