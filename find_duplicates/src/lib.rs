8534a14cf9caa4071b45477433cffbe0e64a085b
Toma Adrian Ionut <sirhadrian21@gmail.com>
2022-11-08 21:04:13 +0200

    moved all logic to lib

--- find_duplicates/src/lib.rs
+++ find_duplicates/src/lib.rs
@@ -1,5 +1,11 @@
-use std::{fs, io, ffi};
 use md5::Digest;
+use std::{
+    collections::HashSet,
+    ffi,
+    fs::{self, ReadDir},
+    io,
+    ffi::OsString,
+};
 
 // Get all the files in a directory
 pub fn get_files_from_dir(path: &str) -> Result<fs::ReadDir, io::Error> {
@@ -12,4 +18,33 @@ pub fn calcuate_md5_sum(file_name: &ffi::OsString) -> Result<Digest, io::Error> 
     let md5_sum = md5::compute(file);
 
     Ok(md5_sum)
+}
+
+// Insert files in hashset
+pub fn find_duplicates(
+    paths: ReadDir,
+    mut hash_set: HashSet<Digest>,
+    mut duplicate_list: Vec<OsString>,
+) {
+    for file in paths {
+        let file = file.expect("Failed to unwrap file from paths");
+        if file
+            .metadata()
+            .expect("Failed to get metadata from file")
+            .is_dir()
+        {
+            continue;
+        }
+
+        let file_name = file.file_name();
+        let md5_sum =
+            calcuate_md5_sum(&file_name).expect("Could not open file to calculate the md5 sum");
+
+        if hash_set.contains(&md5_sum) {
+            duplicate_list.push(file_name.clone());
+            println!("File name: {:?}, md5: {:?}", file_name, md5_sum);
+        } else {
+            hash_set.insert(md5_sum);
+        }
+    }
 }
--- find_duplicates/src/main.rs
+++ find_duplicates/src/main.rs
@@ -1,8 +1,8 @@
 use env_logger;
-use find_duplicates::{calcuate_md5_sum, get_files_from_dir};
+use find_duplicates::{ get_files_from_dir, find_duplicates};
 use log::debug;
 use md5::Digest;
-use std::{collections::HashSet, env};
+use std::{collections::HashSet, env, ffi::OsString};
 
 fn main() {
     env_logger::init();
@@ -11,32 +11,12 @@ fn main() {
     debug!("Supplied arguments: {:?}", args);
 
     let test_dir = args.next().expect("Directory not supplyed");
-    debug!("Supplied direcotry: {}", test_dir);
+    debug!("Supplied direcotry: | {} |", test_dir);
 
-    let mut hash_set: HashSet<Digest> = HashSet::new();
-    let mut duplicate_list = Vec::new();
+    let hash_set: HashSet<Digest> = HashSet::new();
+    let duplicate_list: Vec<OsString> = Vec::new();
 
     let paths = get_files_from_dir(&test_dir).expect("Could not get the files from direcotry");
 
-    for file in paths {
-        let file = file.expect("Failed to unwrap file from paths");
-        if file
-            .metadata()
-            .expect("Failed to get metadata from file")
-            .is_dir()
-        {
-            continue;
-        }
-
-        let file_name = file.file_name();
-        let md5_sum =
-            calcuate_md5_sum(&file_name).expect("Could not open file to calculate the md5 sum");
-
-        if hash_set.contains(&md5_sum) {
-            duplicate_list.push(file_name.clone());
-            println!("File name: {:?}, md5: {:?}", file_name, md5_sum);
-        } else {
-            hash_set.insert(md5_sum);
-        }
-    }
+    find_duplicates(paths, hash_set, duplicate_list);
 }
