2c0d9a80da2ce2639714a53bded59df293dd26c1
Toma Adrian Ionut <sirhadrian21@gmail.com>
2022-11-08 17:12:47 +0200

    args

--- find_duplicates/src/main.rs
+++ find_duplicates/src/main.rs
@@ -1,8 +1,7 @@
 use env_logger;
 use log::debug;
 use md5::Digest;
-use std::{fs, io};
-
+use std::{collections::HashSet, env, fs, io};
 
 // Get all the files in a directory
 // Calculate md5 sum for one file
@@ -10,7 +9,11 @@ use std::{fs, io};
 // Put the duplicates in a separate list
 
 fn main() {
+    env_logger::init();
 
-    
-    
+    let mut args = env::args().skip(1);
+    debug!("Supplied arguments: {}", args);
+
+
+
 }
