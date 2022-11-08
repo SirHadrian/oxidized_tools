e338a92eec327617d84bc0d03ad92759ee3808fd
Toma Adrian Ionut <sirhadrian21@gmail.com>
2022-11-08 17:21:04 +0200

    calculate md5 sum for one file fn

--- find_duplicates/src/main.rs
+++ find_duplicates/src/main.rs
@@ -1,13 +1,19 @@
 use env_logger;
 use log::debug;
 use md5::Digest;
-use std::{collections::HashSet, env, fs, io};
+use std::{collections::HashSet, env, fs, io, ffi};
 
 // Get all the files in a directory
-fn get_files_from_dir(path: &str)-> Result<fs::ReadDir, io::Error>{
+fn get_files_from_dir(path: &str) -> Result<fs::ReadDir, io::Error> {
     Ok(fs::read_dir(path)?)
 }
-// Calculate md5 sum for one file
+// Calculate md5 sum for one file;
+fn calcuate_md5_sum(file_name: ffi::OsString)->Result<Digest, io::Error>{
+    let file =fs::read(file_name)?;
+    let md5_sum=md5::compute(file);
+
+    Ok(md5_sum)
+}
 // Insert the file in hashset
 // Put the duplicates in a separate list
 
@@ -17,12 +23,6 @@ fn main() {
     let mut args = env::args().skip(1);
     debug!("Supplied arguments: {:?}", args);
 
-    let test_dir=args.next().expect("Directory not supplyed");
+    let test_dir = args.next().expect("Directory not supplyed");
     debug!("Supplied direcotry: {}", test_dir);
-
-    
-
-
-
-
 }
