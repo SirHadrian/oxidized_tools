1a4c8f5119355836469719aeb227b6a380b3114c
Toma Adrian Ionut <sirhadrian21@gmail.com>
2022-11-08 17:19:02 +0200

    get files from dir funct

--- find_duplicates/src/main.rs
+++ find_duplicates/src/main.rs
@@ -4,6 +4,9 @@ use md5::Digest;
 use std::{collections::HashSet, env, fs, io};
 
 // Get all the files in a directory
+fn get_files_from_dir(path: &str)-> Result<fs::ReadDir, io::Error>{
+    Ok(fs::read_dir(path)?)
+}
 // Calculate md5 sum for one file
 // Insert the file in hashset
 // Put the duplicates in a separate list
@@ -12,7 +15,13 @@ fn main() {
     env_logger::init();
 
     let mut args = env::args().skip(1);
-    debug!("Supplied arguments: {}", args);
+    debug!("Supplied arguments: {:?}", args);
+
+    let test_dir=args.next().expect("Directory not supplyed");
+    debug!("Supplied direcotry: {}", test_dir);
+
+    
+
 
 
 
